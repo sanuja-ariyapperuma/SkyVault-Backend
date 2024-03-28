a383c278d08e0066b7f8ff8fdbac67051914478a
Sanuja Ariyapperuma <sanuja.a@hsenidbiz.com>
2024-03-28 21:02:24 +0200

    Adding Communication Method Seed
    Adding Communication Method Seed

---
+++ src/SkyVault.WebApi/Backend/Seeds/ComMethodSeedData.cs
@@ -0,0 +1,14 @@
+﻿using SkyVault.WebApi.Backend.Models;
+
+namespace SkyVault.WebApi.Backend.Seeds
+{
+    public class ComMethodSeedData
+    {
+        public static readonly List<CommunicationMethod> comMethod = new List<CommunicationMethod>
+        {
+            new CommunicationMethod { Id = 1, CommTitle = "Non" },
+            new CommunicationMethod { Id = 2, CommTitle = "Email" },
+            new CommunicationMethod { Id = 3, CommTitle = "WhatsApp" }
+        };
+    }
+}
