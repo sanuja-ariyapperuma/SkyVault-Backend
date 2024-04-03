d047fcc23cafb5ed37669d9c47691e2025eb94d9
Sanuja Ariyapperuma <sanuja.a@hsenidbiz.com>
2024-04-03 22:16:01 +0300

    Code refactoring

--- src/SkyVault.WebApi/Backend/CustomerProfileData.cs
+++ src/SkyVault.WebApi/Backend/CustomerProfileData.cs
@@ -5,25 +5,14 @@ using SkyVault.WebApi.Backend.Models;
 
 namespace SkyVault.WebApi.Backend
 {
-    public class CustomerProfileData
+    public class CustomerProfileData(SkyvaultContext db)
     {
-        private readonly SkyvaultContext db;
-
-        public CustomerProfileData(SkyvaultContext db)
-        {
-            this.db = db;
-        }
-
-        public CustomerProfile? Get(string profileId, string systemUserId) 
-        {
-
-            var profile = db.CustomerProfiles.Include(p => p.Passports)
+        public CustomerProfile? Get(int profileId, int systemUserId) => 
+            db.CustomerProfiles.Include(p => p.Passports)
                 .ThenInclude(o => o.Visas)
-                .FirstOrDefault(p => p.Id == Convert.ToInt32(profileId) && p.SystemUserId == Convert.ToInt32(systemUserId));
-            return profile;
+                .FirstOrDefault(p => p.Id == profileId && p.SystemUserId == systemUserId);
    
-        }
-
+ 
         public CustomerProfile Create(CustomerProfile newProfile) 
         {
             var savedprofile = db.CustomerProfiles.Add(newProfile);
