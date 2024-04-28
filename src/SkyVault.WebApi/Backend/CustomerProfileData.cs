13691b7434d352332a971cce1a6e7247a362cdf5
Sanuja Ariyapperuma <54930230+sanuja-ariyapperuma@users.noreply.github.com>
2024-04-29 00:09:28 +0300

    Code refactoring

--- src/SkyVault.WebApi/Backend/CustomerProfileData.cs
+++ src/SkyVault.WebApi/Backend/CustomerProfileData.cs
@@ -13,7 +13,6 @@ namespace SkyVault.WebApi.Backend
             db.CustomerProfiles.Include(p => p.Passports)
                 .ThenInclude(o => o.Visas)
                 .FirstOrDefault(p => p.Id == profileId && p.SystemUserId == systemUserId);
-
         public List<SearchProfileItem>? Search(string searchQuery, int systemUserId)
         {
             string systemUserRoles = db.SystemUsers.Find(systemUserId)!.UserRole!;
@@ -40,8 +39,6 @@ namespace SkyVault.WebApi.Backend
                 p.CustomerProfile.Salutation.SalutationName
             )).ToList();
         }
-
-
         public CustomerProfile Create(CustomerProfile newProfile)
         {
             var savedprofile = db.CustomerProfiles.Add(newProfile);
@@ -49,7 +46,6 @@ namespace SkyVault.WebApi.Backend
 
             return savedprofile.Entity;
         }
-
         public SkyResult<String> ValidateProfile(PassportRequest passportRequest, string correlationId) 
         {
             var isSystemUserExists = db.SystemUsers.Any(s => s.Id == Convert.ToInt32(passportRequest.SystemUserId));
@@ -62,35 +58,34 @@ namespace SkyVault.WebApi.Backend
             {
                 var isParentExists = db.CustomerProfiles.Any(s => s.Id == Convert.ToInt32(passportRequest.ParentId));
                 if (!isParentExists)
-                    return new SkyResult<String>().Fail("Parent does not exist", "4cf0079e-0004", correlationId);
+                    return new SkyResult<String>().Fail("Parent does not exist", "4cf0079e-0001", correlationId);
             }
 
             if (!String.IsNullOrWhiteSpace(passportRequest.CustomerProfileId)) 
             {
                 var isCustomerProfileExists = db.CustomerProfiles.Any(s => s.Id == Convert.ToInt32(passportRequest.CustomerProfileId));
                 if (!isCustomerProfileExists)
-                    return new SkyResult<String>().Fail("Customer Profile does not exist", "4cf0079e-0004", correlationId);
+                    return new SkyResult<String>().Fail("Customer Profile does not exist", "4cf0079e-0002", correlationId);
             }
 
             if (!isSystemUserExists)
-                return new SkyResult<String>().Fail("System User does not exist", "4cf0079e-0000", correlationId);
+                return new SkyResult<String>().Fail("System User does not exist", "4cf0079e-0003", correlationId);
 
             if(!isSalutationExists)
-                return new SkyResult<String>().Fail("Salutation does not exist", "4cf0079e-0001", correlationId);
+                return new SkyResult<String>().Fail("Salutation does not exist", "4cf0079e-0004", correlationId);
 
             if (!isNationalityExists)
-                return new SkyResult<String>().Fail("National does not exist", "4cf0079e-0002", correlationId);
+                return new SkyResult<String>().Fail("National does not exist", "4cf0079e-0005", correlationId);
 
             if (!isCountryExists)
-                return new SkyResult<String>().Fail("Country does not exist", "4cf0079e-0003", correlationId);
+                return new SkyResult<String>().Fail("Country does not exist", "4cf0079e-0006", correlationId);
 
             if (isPassportNumberExists)
-                return new SkyResult<String>().Fail("Passport Number already exists", "4cf0079e-0006", correlationId);
+                return new SkyResult<String>().Fail("Passport Number already exists", "4cf0079e-0007", correlationId);
 
 
             return new SkyResult<String>().SucceededWithValue("Validated");
         }
-
         public SkyResult<CustomerProfile> SaveProfile(PassportRequest passportRequest, string correlationId)
         {
             try
@@ -126,16 +121,15 @@ namespace SkyVault.WebApi.Backend
             }
             catch(Exception ex)
             {
-                return new SkyResult<CustomerProfile>().Fail(ex.Message, "4cf0079e-0005", correlationId);
+                return new SkyResult<CustomerProfile>().Fail(ex.Message, "4cf0079e-0008", correlationId);
             }
         }
-
         public SkyResult<String> UpdateComMethod(ComMethodRequest comMethodRequest, string correlationId) 
         {
             var isCommMethodExists = db.CommunicationMethods.Any(s => s.Id == Convert.ToInt32(comMethodRequest.PrefCommId));
 
             if (!isCommMethodExists)
-                return new SkyResult<String>().Fail("Communication Method does not exist", "4cf0079e-0007", correlationId);
+                return new SkyResult<String>().Fail("Communication Method does not exist", "4cf0079e-0009", correlationId);
 
             var result = db.CustomerProfiles.Where(c => 
                 c.Id == Convert.ToInt32(comMethodRequest.CustomerProfileId) &&
@@ -145,9 +139,28 @@ namespace SkyVault.WebApi.Backend
             );
 
             if (result == 0)
-                return new SkyResult<String>().Fail("No Profile Found or Unauthorize Update", "4cf0079e-0008", correlationId);
+                return new SkyResult<String>().Fail("No Profile Found or Unauthorize Update", "4cf0079e-0010", correlationId);
 
             return new SkyResult<String>().SucceededWithValue("Preffered Commiunication Method Updated");
+        }
+
+        public SkyResult<String> CheckAccessToTheProfile(int customerProfileId, int systemUserId, string correlationId)
+        {
+            var systemUserRole = db.SystemUsers.Find(systemUserId)?.UserRole;
+
+            var result = db.CustomerProfiles.Any(p => 
+                p.Id == customerProfileId && 
+                (
+                    systemUserRole == "admin" ||
+                    systemUserRole == "su.admin" ||
+                    p.SystemUserId == systemUserId
+                )
+            );
+
+            if(!result)
+                return new SkyResult<string>().Fail("Unauthorized", "4cf0079e-0011", correlationId);
+
+            return new SkyResult<string>().SucceededWithValue("Authorized");
         }
     }
 }
