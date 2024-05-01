0c2778f506915a86d59763a8915798ce8bb31788
AyubSourjah <ayub@hsenidbiz.com>
2024-05-01 06:28:46 +0530

    Development done for CustomerProfile creation

--- src/SkyVault.WebApp/Pages/CustomerProfile.cshtml.cs
+++ src/SkyVault.WebApp/Pages/CustomerProfile.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SkyVault.Payloads.RequestPayloads;
 using SkyVault.WebApp.Models;
 using SkyVault.WebApp.Proxies;
 
@@ -9,28 +10,34 @@ namespace SkyVault.WebApp.Pages
     public class CustomerProfileModel(
         IConfiguration configuration,
         IAntiforgery antiForgery,
-        AuthorityProxy authorityProxy) : Models.SkyVaultPageModel(antiForgery)
+        CustomerProxy customerProxy) : Models.SkyVaultPageModel(antiForgery)
     {
         public PassportModel? Passport { get; set; }
         public VisaModel? Visa { get; set; }
         
-        public IActionResult OnGet()
+        public IActionResult OnGet([FromQuery] string? id)
         {
             base.Init();
-            
-            return Page();
-        }
 
-        public IActionResult OnPost([FromHeader] string customerId)
-        {
-            base.Init();
-            
-            //Assign the selected customerid to be pickedup by the roundtrip of the same page
-            TempData["Selected.CustomerId"] = customerId;
-            
-            Response.Headers["HX-Redirect"] = "/customerprofile";
+            if (id == null) return Page();
 
-            return RedirectPermanent("customerprofile");
+            var skyResult = customerProxy.GetCustomerProfile(
+                new GetProfileRequest(id, base.SystemUserId));
+
+            if (skyResult is { Succeeded: true })
+            {
+                TempData["CustomerProfile.ID"] = id;
+                TempData["CustomerProfile.PassportList"] = skyResult.Value?.Passports;
+                
+                return Page();
+            }
+
+            TempData["Message"] = skyResult?.Message;
+            TempData["ErrorCode"] = skyResult?.ErrorCode;
+            TempData["CorrelationId1"] = base.WebCorrelationId;
+            TempData["CorrelationId2"] = skyResult?.CorrelationId;
+
+            return RedirectPermanent("/error");
         }
 
         public IActionResult OnGetPassport(string passportNo)
@@ -41,6 +48,16 @@ namespace SkyVault.WebApp.Pages
         public IActionResult OnGetVisa(string visaNo)
         {
            return Partial("_Visa");
+        }
+
+        public IActionResult OnGetPassportList(string customerId)
+        {
+            return Partial("_PassportList");
+        }
+        
+        public IActionResult OnGetVisaList(string customerId)
+        {
+            return Partial("_VisaList");
         }
     }
 }
