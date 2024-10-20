14c8feddaef37e62a68b28d96d277180709fdfc6
Sanuja Ariyapperuma <54930230+sanuja-ariyapperuma@users.noreply.github.com>
2024-10-20 17:30:38 +0300

    Adding healthcheck, Adding Azure App Insighe,

--- src/SkyVault.WebApi/Program.cs
+++ src/SkyVault.WebApi/Program.cs
@@ -1,5 +1,8 @@
 
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.Tokens;
@@ -10,6 +13,7 @@ using SkyVault.WebApi.Backend.Models;
 using SkyVault.WebApi.Endpoints;
 using SkyVault.WebApi.MappingProfiles;
 using SkyVault.WebApi.Middlewares;
+using System.Text.Json;
 
 namespace SkyVault.WebApi;
 
@@ -21,11 +25,15 @@ public static class Program
 
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddHttpContextAccessor();
-        builder.Services.AddHealthChecks();
+        
         builder.Services.AddDbContext<SkyvaultContext>(options =>
         {
             options.UseMySql(builder.Configuration.GetConnectionString("Localconnection"), new MySqlServerVersion(new Version(8, 0)));
         });
+
+        builder.Services.AddHealthChecks().AddDbContextCheck<SkyvaultContext>(name: "Database");
+
+
         builder.Services.AddAutoMapper(typeof(Program).Assembly,typeof(MappingProfile).Assembly);
 
 
@@ -62,22 +70,37 @@ public static class Program
             options.AddPolicy(MyAllowSpecificOrigins,
                 builder =>
                 {
-                    builder.WithOrigins("http://localhost:5173")
+                    builder.WithOrigins("http://localhost:5173", "https://witty-wave-03975c200.5.azurestaticapps.net")
                            .AllowAnyHeader()
                            .AllowAnyMethod()
                            .AllowCredentials();
                 });
         });
 
-       
+
+
+        //Log.Logger = new LoggerConfiguration()
+        //.ReadFrom.Configuration(builder.Configuration)
+        //.MinimumLevel.Information()
+        //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        //.Enrich.FromLogContext()
+        //.WriteTo.File(builder.Configuration["Logging:FilePath"]!, rollingInterval: RollingInterval.Day)
+        //.WriteTo.Console()
+        //.CreateLogger();
 
         Log.Logger = new LoggerConfiguration()
-        .ReadFrom.Configuration(builder.Configuration)
-        .MinimumLevel.Information()
-        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-        .Enrich.FromLogContext()
-        .WriteTo.File(builder.Configuration["Logging:FilePath"]!, rollingInterval: RollingInterval.Day)
-        .CreateLogger();
+            .WriteTo.ApplicationInsights(
+            new TelemetryConfiguration { InstrumentationKey = builder.Configuration["AzureAppInsight:InstrumentKey"] },
+            TelemetryConverter.Traces
+            )
+            .MinimumLevel.Information()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .WriteTo.File(builder.Configuration["Logging:FilePath"]!, rollingInterval: RollingInterval.Day)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Information("API is starting up");
 
         builder.Host.UseSerilog();
 
@@ -87,11 +110,31 @@ public static class Program
         app.MapLoginEndpoints();
         app.MapCustomEndpoints();
         app.MapProfileEndpoints();
-        app.MapHealthChecks("/health");
         app.UseCors(MyAllowSpecificOrigins);
         app.UseAuthentication();
         app.UseAuthorization();
         //app.UseMiddleware<ExceptionMiddleware>();
+
+        app.MapHealthChecks("/api/health", new HealthCheckOptions
+        {
+            ResponseWriter = async (context, report) =>
+            {
+                context.Response.ContentType = "application/json";
+                var result = JsonSerializer.Serialize(new
+                {
+                    status = report.Status.ToString(),
+                    checks = report.Entries.Select(e => new
+                    {
+                        name = e.Key,
+                        status = e.Value.Status.ToString(),
+                        exception = e.Value.Exception != null ? e.Value.Exception.Message : null,
+                        duration = e.Value.Duration.TotalMilliseconds
+                    })
+                });
+                await context.Response.WriteAsync(result);
+            }
+        });
+
         app.Run();
     }
 }
