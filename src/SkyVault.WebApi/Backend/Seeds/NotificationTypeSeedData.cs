22c1aa90eefb35f0c7de45d835876a0c75618b41
Sanuja Ariyapperuma <54930230+sanuja-ariyapperuma@users.noreply.github.com>
2025-02-18 22:26:51 +0200

    Adding seed data to NotificationTypes

---
+++ src/SkyVault.WebApi/Backend/Seeds/NotificationTypeSeedData.cs
@@ -0,0 +1,15 @@
+﻿using SkyVault.WebApi.Backend.Models;
+
+namespace SkyVault.WebApi.Backend.Seeds
+{
+    public sealed class NotificationTypeSeedData
+    {
+        public static readonly List<NotificationType> notifications =
+        [
+            new NotificationType { Id = 1, TypeName = "Birthday"},
+            new NotificationType { Id = 2, TypeName = "PassportExpiry"},
+            new NotificationType { Id = 3, TypeName = "VisaExpiry"},
+            new NotificationType { Id = 4, TypeName = "Custom"},
+        ];
+    }
+}
