dcef0b22ce52c28d7ab92ef4c5a2c20bca61ab79
Sanuja Ariyapperuma <54930230+sanuja-ariyapperuma@users.noreply.github.com>
2025-03-30 12:42:36 +0300

    ActionFilter implemented for authorization
    For the endpoints only admin and superadmin roles are authorized, an filter implemented to filter them.

--- src/SkyVault.WebApi/Endpoints/MessageEndpoints.cs
+++ src/SkyVault.WebApi/Endpoints/MessageEndpoints.cs
@@ -1,22 +1,32 @@
-﻿namespace SkyVault.WebApi.Endpoints
+﻿using Microsoft.AspNetCore.Http;
+using SkyVault.WebApi.Filters;
+
+namespace SkyVault.WebApi.Endpoints
 {
     internal static class MessageEndpoints
     {
+        /**
+         * When defining endpoints order is important. 
+         * First check the authentication and then authorization always and so on for other filters.
+         */
         public static void MapMessageEndpoints(this WebApplication app)
         {
             app.MapPost("/GetPreSignedUrl", Workloads.MessageWorkload.GetPreSignedUrl)
                 .RequireAuthorization()
+                .AddEndpointFilter<AdminOnlyFilter>()
                 .Produces(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status401Unauthorized);
 
 
             app.MapPost("/UpdateAttachmentFile", Workloads.MessageWorkload.UpdateAttachmentFile)
                 .RequireAuthorization()
+                .AddEndpointFilter<AdminOnlyFilter>()
                 .Produces(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status401Unauthorized);
 
             app.MapGet("/GetMessage", Workloads.MessageWorkload.GetMessage)
                 .RequireAuthorization()
+                .AddEndpointFilter<AdminOnlyFilter>()
                 .Produces(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status401Unauthorized);
 
