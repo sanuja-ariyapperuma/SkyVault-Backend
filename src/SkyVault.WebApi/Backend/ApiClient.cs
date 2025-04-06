ff9fe48f3e48404b7f8ce1ce8857e9e981993de6
Sanuja Ariyapperuma <54930230+sanuja-ariyapperuma@users.noreply.github.com>
2025-04-06 16:53:00 +0300

    missing file

--- src/SkyVault.WebApi/Backend/ApiClient.cs
+++ src/SkyVault.WebApi/Backend/ApiClient.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using Azure.Core;
+using Azure.Identity;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 
 namespace SkyVault.WebApi.Backend
@@ -20,11 +24,22 @@ namespace SkyVault.WebApi.Backend
                 PromotionType = promotionalType
             };
 
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetCredentialsAsync());
+
             var content = new StringContent(JsonSerializer.Serialize(requestBody), Encoding.UTF8, "application/json");
 
             var response = await _httpClient.PostAsync("/api/PromotionalHTTPFunction", content);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
+        }
+
+        private async Task<string> GetCredentialsAsync() 
+        {
+            var scope = "api://371a1a76-a0cc-46f3-8318-8f91058f3cef/.default";
+            var credential = new DefaultAzureCredential();
+            var token = await credential.GetTokenAsync(new TokenRequestContext(new[] { scope }));
+
+            return token.Token;
         }
     }
 }
