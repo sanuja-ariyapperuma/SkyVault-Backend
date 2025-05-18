c510ef0b4cf7e3ac994386cba3c220ad49d5c6f7
Sanuja Ariyapperuma <54930230+sanuja-ariyapperuma@users.noreply.github.com>
2025-05-18 16:17:59 +0300

    EmailAccountStatus

--- src/SkyVault.WebApi/Backend/IApiClient.cs
+++ src/SkyVault.WebApi/Backend/IApiClient.cs
@@ -1,7 +1,10 @@
-﻿namespace SkyVault.WebApi.Backend
+﻿using SkyVault.Payloads.ResponsePayloads;
+
+namespace SkyVault.WebApi.Backend
 {
     public interface IApiClient
     {
         Task<string> PostBroardcastMessageAsync(int templateId, string promotionalType);
+        Task<EmailAccountInfo> GetEmailAccountInformation();
     }
 }
---
+++ src/SkyVault/Payloads/ResponsePayloads/EmailAccountInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyVault.Payloads.ResponsePayloads
+{
+    public record EmailAccountInfo(string CompanyName, string CompanyEmailAddress, Plan[] Plans);
+    public record Plan(string Type, string CreditsType, int Credits);
+}
